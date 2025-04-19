2f0af067e13f159fd3c92d83d41a4c450e39b800
Daniil <den.sereda16@gmail.com>
2025-04-19 18:00:15 +0200

    Some fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        // sans: ["Inter", "sans-serif"],
+        sans: ["Inter", "sans-serif"],
         sans: ["Poppins", ...fontFamily.sans],
       },
       screens: {
