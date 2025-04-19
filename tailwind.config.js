cdbf5ba0719a2f4e2a418ba02e817bf4cdeeecdb
Daniil <den.sereda16@gmail.com>
2025-04-19 18:06:41 +0200

    Some fix in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ export default {
     extend: {
       fontFamily: {
         sans: ["Inter", "sans-serif"],
-        sans: ["Poppins", ...fontFamily.sans],
       },
       screens: {
         xl: "1400px",
